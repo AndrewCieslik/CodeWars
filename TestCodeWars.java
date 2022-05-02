81a745d0952e22fd58c350380898c3af6f1ea95c
andrew <moto.cieslik@gmail.com>
2022-05-02 22:46:14 +0200

    CodeWars Array Practice Kyu8

---
+++ TestCodeWars.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestCodeWars {
+    public static void main(String[] args) {
+        int n =1234;
+        int length = String.valueOf(n).length();
+        String number = String.valueOf(n);
+
+        String reverse = new StringBuilder(new String(number)).reverse().toString();
+        char[] charReverseArray = reverse.toCharArray();
+
+        String raw = "1233983543587325318";
+        char[] c = raw.toCharArray();
+        int[] reverseIntArray = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            reverseIntArray[i] = (int)charReverseArray[i] - 48;
+        }
+        System.out.println(reverseIntArray[3]);
+    }
+
+
+}
